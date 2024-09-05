f1c30c4a87f318ddd7081f41d35bffaa6e332cc6
Brad Hendrickson <brad.hendrickson@silvertrek.net>
2024-09-05 09:47:59 -0700

    fix homepage formatting for real

--- app/page.jsx
+++ app/page.jsx
@@ -20,19 +20,23 @@ export default async function Index() {
 			>
 				<Col xs="auto">
 					<h2 className="mt-5">Welcome to Moonstone.</h2>
-					{user ? (
-						<h5>
-							Simple time tracking that stays out of your way.{' '}
-						</h5>
-					) : (
-						<h5>Create an account to begin tracking your project time!</h5>
-            
-					)}
-          <ul>
-            <li><Link href="/tracker">Time Tracker</Link>: record what you work on</li>
-            <li><Link href="/weeklysummary">Weekly Summary</Link>: see what you worked on each week by project or day</li>
-			<li><Link href="/projects">Projects</Link>: manage your projects</li>
-          </ul>
+					{user ? <h5>Simple time tracking that stays out of your way. </h5> : <h5>Create an account to begin tracking your project time!</h5>}
+					<ul>
+						<li>
+							<Link href="/tracker">Time Tracker</Link>: record what you work on
+						</li>
+						<li>
+							<Link href="/weeklysummary">Weekly Summary</Link>: see what you worked on each week by project or day
+						</li>
+						<li>
+							<Link href="/projects">Projects</Link>: manage your projects
+						</li>
+					</ul>
+
+					<h5 className="mt-5">🐛 Found a bug?</h5>
+					<p>
+						Please report it on the <Link href="https://github.com/BradleyHendrickson/Moonstone/issues">Issue Board</Link>
+					</p>
 				</Col>
 				<Col
 					xs="auto"
@@ -42,18 +46,6 @@ export default async function Index() {
 					<Image priority src={logoBlackSvg} alt="Moonstone" width={100} />
 				</Col>
 			</Row>
-
-			<Row className="justify-content-center">
-				<Col>
-					<h5 className='mt-3'>
-						🐛 Found a bug? 
-					</h5>
-					<p>
-						Please report it on the <Link href="https://github.com/BradleyHendrickson/Moonstone/issues">Issue Board</Link>
-					</p>
-				</Col>
-			</Row>
-
 			<div className="footer-text">Chatoyant Solutions</div>
 		</Container>
 	);
