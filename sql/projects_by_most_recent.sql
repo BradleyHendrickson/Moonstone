52e82576edf11cac697eb6b55d19bc2c3b798967
Brad Hendrickson <brad.hendrickson@silvertrek.net>
2024-09-11 15:55:51 -0700

    add sql for most recent project

--- sql/projects_by_most_recent.sql
+++ sql/projects_by_most_recent.sql
@@ -1,28 +1,32 @@
-create or replace view projects_by_most_recent as
-select 
-    p.id, 
-    p.created_at, 
-    p.name, 
-    p.description, 
-    p.status, 
-    p.user_id, 
-    p.billable, 
-    p.seq, 
-    p.hidden,
-    coalesce(w.max_start_time, p.created_at) as last_used,
-    (w.max_start_time is null) as "new"
-from 
-    projects p
-left join (
-    select 
-        project_id, 
-        user_id,
-        max(start_time) as max_start_time
-    from 
-        worksession
-    group by 
-        project_id, user_id
-) w on p.id = w.project_id and p.user_id = w.user_id
+DROP VIEW IF EXISTS public.projects_by_most_recent;
 
-order by 
-    coalesce(w.max_start_time, p.created_at) desc;
+CREATE VIEW
+  public.projects_by_most_recent WITH (security_invoker) AS
+SELECT
+  p.id,
+  p.created_at,
+  p.name,
+  p.description,
+  p.status,
+  p.user_id,
+  p.billable,
+  p.seq,
+  p.hidden,
+  COALESCE(w.max_start_time, p.created_at) AS last_used,
+  (w.max_start_time IS NULL) AS "new"
+FROM
+  projects p
+  LEFT JOIN (
+    SELECT
+      project_id,
+      user_id,
+      MAX(start_time) AS max_start_time
+    FROM
+      worksession
+    GROUP BY
+      project_id,
+      user_id
+  ) w ON p.id = w.project_id
+  AND p.user_id = w.user_id
+ORDER BY
+  COALESCE(w.max_start_time, p.created_at) DESC;
